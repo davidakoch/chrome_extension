45f823f5b27bd0afa0393188c7ef37eaac45d1f7
David A Koch <kodave1@gmail.com>
2017-11-18 14:16:32 -0500

    fixed up code in popup js

--- hello_world/popup.js
+++ hello_world/popup.js
@@ -8,8 +8,6 @@ document.getElementById("name").addEventListener("keyup", myFunction);
 
 function myFunction() {
     console.log("hello")
-    //var x = document.getElementById("name");
-    //x.value = x.value.toUpperCase();
     document.getElementById("greet").innerHTML = "Hello " + document.getElementById("name").value;
 }
 
