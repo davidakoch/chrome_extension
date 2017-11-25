84cf30292a36c2f10f785607f0ab269ce084d934
David A Koch <kodave1@gmail.com>
2017-11-25 17:45:32 -0500

    changed get total to javascript

--- budget_manager/popup.js
+++ budget_manager/popup.js
@@ -4,13 +4,23 @@ $(function(){
 		$('#total').text(budget.total);
 	})
 
-	//get element by class...then add a click event
-	$('#spendAmount').click(function(){
+		//get element by class...then add a click event
+		/*$('#spendAmount').click(function(){
+		chrome.storage.sync.get('total', function(budget){
+			var newTotal = 0;
+			if (budget.total){
+				newTotal += parseInt(budget.total);
+			}*/
+		//here is pure javascript code...not jquery	
+		document.getElementById("spendAmount").onclick = 
+  		function(){
 		chrome.storage.sync.get('total', function(budget){
 			var newTotal = 0;
 			if (budget.total){
 				newTotal += parseInt(budget.total);
 			}
+			
+
 			//create a variable that holds the amount value
 			//...what's the pure javascript equivalent of .val
 			var amount = $('#amount').val();
@@ -23,5 +33,5 @@ $(function(){
 			$('#total').text(newTotal);
 			$('#amount').val('')
 		})
-	})
+	}
 });
