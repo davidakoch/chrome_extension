0e9fa863a6358d386d6942e715e3f179ae7f1836
David A Koch <kodave1@gmail.com>
2017-11-28 12:59:07 -0500

    added innerhtml and fixed u code

--- budget_manager/popup.js
+++ budget_manager/popup.js
@@ -4,13 +4,6 @@ $(function(){
 		$('#total').text(budget.total);
 	})
 
-		//get element by class...then add a click event
-		/*$('#spendAmount').click(function(){
-		chrome.storage.sync.get('total', function(budget){
-			var newTotal = 0;
-			if (budget.total){
-				newTotal += parseInt(budget.total);
-			}*/
 		//here is pure javascript code...not jquery	
 		document.getElementById("spendAmount").onclick = 
   		function(){
@@ -19,20 +12,17 @@ $(function(){
 			if (budget.total){
 				newTotal += parseInt(budget.total);
 			}
-			
-
-			//create a variable that holds the amount value
-			//...what's the pure javascript equivalent of .val
 
 			var amount = document.getElementById("amount").value;
 			if(amount){
 				newTotal += parseInt(amount);
 			}
+
 			chrome.storage.sync.set({'total': newTotal});
 
-			//find the javascript equivalent of text
-			$('#total').text(newTotal);
+			document.getElementById("total").innerHTML = newTotal;
 			$('#amount').val('')
+	
 		})
 	}
 });
