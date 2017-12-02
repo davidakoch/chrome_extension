564526ef76a1baa5b1081b7434996bf271543d24
David A Koch <kodave1@gmail.com>
2017-12-02 00:15:48 -0500

    finished function to close tab with limit

--- budget_manager/options.js
+++ budget_manager/options.js
@@ -1,5 +1,10 @@
 $(function(){
 	$('#saveLimit').click(function(){
 		var limit = $('#limit').val();
+		if(limit){
+			chrome.storage.sync.set({'limit': limit}, function(){
+				close();
+			})
+		}
 	})
 })
